7e343994da5683b2d48a25cbb3a85526abfabc1b
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-01 17:04:54 +0800

    新增 注册时验证邮箱。

--- api/user/__init__.py
+++ api/user/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from user import type
+from user import type,verify_content
 
 user = APIRouter(
     prefix="/user",
@@ -8,8 +8,21 @@ user = APIRouter(
 
 @user.post("/register")
 def register(data: type.register):
+    # 邮箱判断
+    if verify_content.email(data.email) == False :
+        return_content = {
+            "code": 500,
+            "message": "邮箱格式错误"
+        }
+        return return_content
+    
     return data
 
 @user.post("/login")
 def login(data: type.login):
-    return data
+    # 验证逻辑 没写完
+    return_content = {
+        "code": 500,
+        "message": "用户名或密码错误"
+    }
+    return return_content
--- api/user/type.py
+++ api/user/type.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 class login(BaseModel):
     username: str
     password: str
+
+class register(BaseModel):
+    email: str
+    avatar: str
+    username: str
+    password: str
