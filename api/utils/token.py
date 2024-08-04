028fcd0bd196d2d5d527588b92a09935d4c7c225
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-05 00:43:21 +0800

    修复 配置文件缺失字段。

--- api/utils/token.py
+++ api/utils/token.py
@@ -1,17 +1,17 @@
 import config
 import jwt
 
-auth = config.get()["auth"]
+auth = config.get("auth")
 secret_key = auth["secret_key"]
 
 def create(payload: dict):
     # 生成JWT并指定算法
-    token = jwt.encode(payload, secret_key, algorithm=auth["algorithm"])
+    token = jwt.encode(payload, secret_key, algorithm=[auth["algorithm"]])
     return token
 
 def verify(received_token):
     try:
-        payload = jwt.decode(received_token, secret_key, algorithms=['HS256'])
+        payload = jwt.decode(received_token, secret_key, algorithms=[auth["algorithm"]])
         return True, payload, "成功"
     except jwt.ExpiredSignatureError:
         return False, {}, "token过期"
