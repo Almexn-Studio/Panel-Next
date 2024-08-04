06be18baa7433b76e966c5d2278cebb0c4d6c050
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-05 00:45:19 +0800

    修复 Key is none.

--- api/config.py
+++ api/config.py
@@ -5,4 +5,6 @@ config = toml.load("config.toml")
 def get(group=None,key=None):
     if group is None:
         return config[key]
+    if key is None:
+        return config[group]
     return config[group][key]
