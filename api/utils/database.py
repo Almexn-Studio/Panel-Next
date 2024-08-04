44b9aa328bfd370a03cc1778e64664f0803c78a3
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-04 23:31:45 +0800

    新增 数据库-增加文档。

--- api/utils/database.py
+++ api/utils/database.py
@@ -1,5 +1,13 @@
 from pymongo import MongoClient
 import config
 
-mongodb_url = config.get('database','url')
-client = MongoClient()
+mongodb_url = "mongodb://"
+mongodb_url += config.get('database','username')
+mongodb_url += ":"+config.get('database','password')
+
+client = MongoClient(mongodb_url)
+db = client["runoobdb"]
+
+def add_document(collections:str, document:list):
+    col = db[collections]
+    return col.insert_many(document)
