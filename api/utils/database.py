b49dddafa1dcca778f2fc224d3c922c139a46434
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-04 23:34:48 +0800

    新增 数据库操作。

--- api/utils/database.py
+++ api/utils/database.py
@@ -11,3 +11,15 @@ db = client["runoobdb"]
 def add_document(collections:str, document:list):
     col = db[collections]
     return col.insert_many(document)
+
+def get_document(collections:str, query:dict):
+    col = db[collections]
+    return col.find(query)
+
+def update_document(collections:str, query:dict, new_data:dict):
+    col = db[collections]
+    return col.update_many(query, new_data)
+
+def delete_document(collections:str, query:dict):
+    col = db[collections]
+    return col.delete_many(query)
