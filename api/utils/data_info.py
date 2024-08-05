3d51c965386bf217f2b86ba7f748763f5992597e
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-08-06 00:44:35 +0800

    修复 文件不存在。

--- api/utils/data_info.py
+++ api/utils/data_info.py
@@ -1,7 +1,9 @@
 import json
+import os
 
 def get(dir):
-    dir = "/data/"+dir
-    file = open(dir, "r")
+    print(os.getcwd())
+    dir = os.getcwd()+"/data/"+dir
+    file = open(dir, "r", encoding="utf-8")
     data = json.load(file)
     return data
